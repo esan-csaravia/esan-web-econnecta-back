93d45fea4dcdcabab2944481e43efc0abcbd8e34
ronaldohr <ronaldo.hanccor@gmail.com>
2025-12-05 03:07:53 -0500

    Añadido sistema de gestión de bloqueos de usuarios
    Se implementaron las siguientes funcionalidades:
    - Creación de la clase `BloqueosUsuariosDTO` con subclases para manejar datos de creación y listado de bloqueos.
    - Definición de las interfaces `IBloqueosUsuariosRepository` e `IBloqueosUsuariosService` para la gestión de bloqueos.
    - Implementación de la clase `BloqueosUsuariosService` con métodos para crear bloqueos, desactivar usuarios y obtener historiales.
    - Implementación de la clase `BloqueosUsuariosRepository` para interactuar con la base de datos.
    - Creación del controlador `BloqueosUsuariosController` con endpoints para crear bloqueos y consultar historiales.

---
+++ Web.EcoConecta/Web.EcoConecta.CORE/Core/DTOs/BloqueosUsuariosDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.EcoConecta.CORE.Core.DTOs
+{
+    public class BloqueosUsuariosDTO
+    {
+        public class CreateBloqueoDTO
+        {
+            public int IdUsuario { get; set; }
+            public int IdAdmin { get; set; }
+            public string Motivo { get; set; }
+        }
+
+        public class BloqueoListDTO
+        {
+            public int IdBloqueo { get; set; }
+            public int IdUsuario { get; set; }
+            public int IdAdmin { get; set; }
+            public string Motivo { get; set; }
+            public DateTime? Fecha { get; set; }
+
+            public string NombreUsuario { get; set; }
+            public string NombreAdmin { get; set; }
+        }
+    }
+}
+
---
+++ Web.EcoConecta/Web.EcoConecta.CORE/Infraestructure/Repositories/BloqueosUsuariosRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web.EcoConecta.CORE.Core.Entities;
+using Web.EcoConecta.CORE.Core.Interfaces;
+using Web.EcoConecta.CORE.Infraestructure.Data;
+
+namespace Web.EcoConecta.CORE.Infraestructure.Repositories
+{
+    public class BloqueosUsuariosRepository : IBloqueosUsuariosRepository
+    {
+        private readonly EcoConectaDbContext _context;
+
+        public BloqueosUsuariosRepository(EcoConectaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CrearAsync(BloqueosUsuarios bloqueo)
+        {
+            await _context.BloqueosUsuarios.AddAsync(bloqueo);
+            await _context.SaveChangesAsync();
+            return bloqueo.IdBloqueo;
+        }
+
+        public async Task<IEnumerable<BloqueosUsuarios>> GetByUsuarioAsync(int idUsuario)
+        {
+            return await _context.BloqueosUsuarios
+                .Where(b => b.IdUsuario == idUsuario)
+                .Include(b => b.IdUsuarioNavigation)
+                .Include(b => b.IdAdminNavigation)
+                .OrderByDescending(b => b.Fecha)
+                .ToListAsync();
+        }
+    }
+}
+
